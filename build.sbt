dd223e2d65032b7cb513eaf6e0ffcf5f7cf5fe4d
akshay <akshaymh@gslab.com>
2016-09-10 19:34:10 +0530

    Adding basic setup
    Adding basic setup

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "scala-00"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
---
+++ src/main/scala-2.11/com/learning/workshop/Basics.scala
@@ -0,0 +1,10 @@
+package com.learning.workshop
+
+/**
+  * Created by Akshay Mhetre on 9/10/2016.
+  */
+object Basics {
+  def main(args: Array[String]): Unit = {
+    println("Hello world")
+  }
+}
