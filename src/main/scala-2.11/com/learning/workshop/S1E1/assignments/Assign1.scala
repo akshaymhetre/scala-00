c117ec2a1d8c97e1d672fb3324ee41bd93506798
akshay <akshaymh@gslab.com>
2016-11-30 16:11:03 +0530

    Adding new packages.

---
+++ src/main/scala-2.11/com/learning/workshop/S1E1/assignments/Assign1.scala
@@ -0,0 +1,8 @@
+package com.learning.workshop.S1E1.assignments
+
+/**
+  * Created by Akshay Mhetre on 11/30/2016.
+  */
+object Assign1 {
+
+}
---
+++ src/main/scala-2.11/com/learning/workshop/S1E1/practicals/Practical1.scala
@@ -0,0 +1,8 @@
+package com.learning.workshop.S1E1.practicals
+
+/**
+  * Created by Akshay Mhetre on 11/30/2016.
+  */
+object Practical1 {
+
+}
