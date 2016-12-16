0649cb39559c44e0003b525ccef8107a6f0acd85
akshay <akshaymh@gslab.com>
2016-12-16 15:37:02 +0530

    S01E01 code done.

--- src/main/scala-2.11/com/learning/myAttempts/S1E5/Practical5.scala
+++ src/main/scala-2.11/com/learning/myAttempts/S1E5/Practical5.scala
@@ -1,5 +1,7 @@
 package com.learning.myAttempts.S1E5
 
+import com.learning.myAttempts.S1E5.Ordering.Ord
+
 /**
   * Created by Akshay Mhetre on 12/16/2016.
   */
@@ -15,3 +17,58 @@ object Practical5 extends App {
     case _ => elem :: xs
   }
 }
+
+object Ordering {
+
+}
+
+// Write insertion sort on list of integers
+object InsertionSortForInt{
+  def insert(x: Int, xs: List[Int]): List[Int] = xs match {
+    case Nil                             => List(x)
+    case head :: tail if head < x => head :: insert(x, tail)
+    case head :: tail                    => x :: xs
+  }
+
+  def sort(xs: List[Int]): List[Int] = xs match {
+    case Nil          => Nil
+    case head :: tail => insert(head, sort(tail))
+  }
+
+  sort(List(2,5,1,3))
+}
+
+// Make it generic
+object InsertionSortGen{
+  trait Ord[T] {
+    def lt(a: T, b: T): Boolean
+  }
+
+  object Ord {
+    val intOrd: Ord[Int] = new Ord[Int] {
+      def lt(a: Int, b: Int): Boolean = a < b
+    }
+
+    val strOrd: Ord[String] = new Ord[String] {
+      def lt(a: String, b: String): Boolean = a < b
+    }
+
+  }
+
+  object Sorter {
+    private def insert[T](x: T, xs: List[T])(ord: Ord[T]): List[T] = xs match {
+      case Nil                             => List(x)
+      case head :: tail if ord.lt(head, x) => head :: insert(x, tail)(ord)
+      case head :: tail                    => x :: xs
+    }
+
+    def sort[T](xs: List[T])(ord: Ord[T]): List[T] = xs match {
+      case Nil          => Nil
+      case head :: tail => insert(head, sort(tail)(ord))(ord)
+    }
+  }
+
+  Sorter.sort[Int](List(1,2,3,5,2))(Ord.intOrd)
+
+}
+
