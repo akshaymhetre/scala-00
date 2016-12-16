4b06384e804ad1c299415ab55b13714b0b9bffbe
akshay <akshaymh@gslab.com>
2016-12-16 16:16:25 +0530

    S01E01 code done.

--- src/main/scala-2.11/com/learning/myAttempts/S1E5/Practical5.scala
+++ src/main/scala-2.11/com/learning/myAttempts/S1E5/Practical5.scala
@@ -1,11 +1,11 @@
 package com.learning.myAttempts.S1E5
 
-import com.learning.myAttempts.S1E5.Ordering.Ord
+
 
 /**
   * Created by Akshay Mhetre on 12/16/2016.
   */
-object Practical5 extends App {
+object Practical5 {
   def sort(xs: List[Int]):List[Int] = xs match {
     case head :: tail => insert(head, sort(tail))
     case Nil => xs
@@ -23,7 +23,7 @@ object Ordering {
 }
 
 // Write insertion sort on list of integers
-object InsertionSortForInt{
+object InsertionSortForInt extends App{
   def insert(x: Int, xs: List[Int]): List[Int] = xs match {
     case Nil                             => List(x)
     case head :: tail if head < x => head :: insert(x, tail)
@@ -39,7 +39,7 @@ object InsertionSortForInt{
 }
 
 // Make it generic
-object InsertionSortGen{
+object InsertionSortGen1 extends App{
   trait Ord[T] {
     def lt(a: T, b: T): Boolean
   }
@@ -51,6 +51,22 @@ object InsertionSortGen{
 
     val strOrd: Ord[String] = new Ord[String] {
       def lt(a: String, b: String): Boolean = a < b
+    }
+
+    def optOrd[T](ord: Ord[T]):Ord[Option[T]] = new Ord[Option[T]] {
+      def lt(a: Option[T], b: Option[T]): Boolean = (a, b) match {
+        case (Some(a), Some(b)) => ord.lt(a,b)
+        case (None, _) => true
+        case _ => false
+      }
+    }
+
+    def tupleOrd[T1, T2](ord1: Ord[T1], ord2: Ord[T2]): Ord[(T1, T2)] = new Ord[(T1, T2)] {
+      def lt(a: (T1, T2), b: (T1, T2)): Boolean = {
+        if (ord1.lt(a._1, b._1)) true
+        else if (ord1.lt(b._1, a._1)) false
+        else ord2.lt(a._2, b._2)
+      }
     }
 
   }
@@ -68,7 +84,14 @@ object InsertionSortGen{
     }
   }
 
-  Sorter.sort[Int](List(1,2,3,5,2))(Ord.intOrd)
+  Sorter.sort[Int](List(1,2,3,5,2))(Ord.intOrd).foreach(println)
+
+  Sorter.
+    sort[Option[Int]](List(Some(1),None, Some(0)))(Ord.optOrd(Ord.intOrd)).foreach(println)
+
+  val lstOfTup = List((5,"akshay"),(2,"rahul"))
+
+  Sorter.sort[(Int, String)](lstOfTup)(Ord.tupleOrd(Ord.intOrd,Ord.strOrd))
 
 }
 
